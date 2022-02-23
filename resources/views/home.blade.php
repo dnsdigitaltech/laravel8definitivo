fa28afe14e88933f89ed17154051f752af42bee3
dnsdigitaltech <dnssites@gmail.com>
2022-02-23 09:47:22 -0300

    Configurações de banco de dados

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.template')
+@section('title', 'Página inicial')
+@section('content')
+<h1>Página Home</h1>
+@endsection
---
+++ resources/views/produtos/create.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.template')
+@section('title', 'Página produto')
+@section('content')
+<h1>Página de criar produto</h1>
+@endsection
